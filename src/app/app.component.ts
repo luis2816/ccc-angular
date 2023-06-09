5a636cbb161461e38b79851a05c4e2006b20c7be
pablo_alejandro <pablo_alejandro@bitbucket.org>
2023-06-09 14:50:20 -0500

    control de accesibilidad en headerTopBar, Pqrs page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { GlobalService } from 'src/app/services/global.service';
 import { Component  } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent  {
   componentVisible = true;
 
@@ -16,5 +16,4 @@ export class AppComponent  {
   toggleComponent() {
     this.componentVisible = !this.componentVisible;
   }
-  
 }
--- src/app/components/componentes/header/header.component.ts
+++ src/app/components/componentes/header/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private elementRef: ElementRef){}
 
+  rootFontSize = 16;
 
+  showControls(){
+    const controls = this.elementRef.nativeElement.querySelector('.accesibility');
+    if(!controls.classList.contains('show'))controls.classList.add('show');
+    else controls.classList.remove('show');
+  }
+  changeFontSize(size:boolean){
+    const rootElm = document.documentElement;
+    if(size){
+      if(this.rootFontSize < 20)this.rootFontSize++;
+    }else{
+      if(this.rootFontSize > 16)this.rootFontSize--;
+    }
+    rootElm.style.setProperty('font-size', this.rootFontSize + 'px');
+  }
 }
